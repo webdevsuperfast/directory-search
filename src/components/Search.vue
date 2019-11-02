f5e50d7c81123fda6a6b0b92e81b379fd29fe708
webdevsuperfast <acob.rotsen@hotmail.com>
2019-11-02 16:38:08 +0800

    Code changes and cleanup

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,18 +1,22 @@
 <template>
   <main class="mx-auto my-auto">
-    <b-form inline>
+    <b-form inline @submit.prevent="handleSubmit">
       <label class="sr-only" for="search-input">Search String</label>
-      <b-form-input id="search-input" type="text" placeholder="keyword"></b-form-input>
+      <b-form-input 
+        name="keyword" 
+        v-model="api.keyword" 
+        id="search-input" type="text" 
+        placeholder="keyword" 
+        ></b-form-input>
       <label class="sr-only" for="search-select"></label>
       <b-form-select
         id="search-select"
         class="ml-2"
         :value="null"
         :options="filetypes"
+        v-model.trim="filetype"
+        v-on:change="getFiletype"
       >
-      <template v-slot:first>
-        <option :value="null">Choose</option>
-      </template>
       </b-form-select>
       <b-button variant="primary" class="ml-2">Search</b-button>
     </b-form>
@@ -20,14 +24,19 @@
 </template>
 
 <script>
+import { validationMixin } from 'vuelidate'
+import { required, minLength } from 'vuelidate/lib/validators'
+
 export default {
   name: 'Search',
+  mixins: [validationMixin],
   data() {
     return {
-      query: '?q=',
-      site: 'https://www.google.com/search',
-      delimit: ' +intitle:%22index of%22 -inurl:(jsp|pl|php|html|aspx|htm|cf|shtml)',
-      exclusions: ' -inurl:(hypem|unknownsecret|sirens|writeups|trimediacentral|articlescentral|listen77|mp3raid|mp3toss|mp3drug|theindexof|index_of|wallywashis|indexofmp3)',
+      filetype: 'music',
+      form: {
+        keyword: '',
+        filetype: 'music'
+      },
       filetypes: {
         music: 'Music',
         flac: 'Lossless Music',
@@ -35,9 +44,63 @@ export default {
         ebook: 'E-Book',
         kindle: 'Kindle',
         image: 'Images',
-        archive: 'Archive',
-        torrent: 'Torrent' 
+        archive: 'Archive'
+        // torrent: 'Torrent' 
+      },
+      api: {
+        baseUrl: 'https://www.google.com/search?q=',
+        formats: '',
+        keyword: '',
+        inTitle: '%22index of%22',
+        inUrl: '(jsp|pl|php|html|aspx|htm|cf|shtml|hypem|unknownsecret|sirens|writeups|trimediacentral|articlescentral|listen77|mp3raid|mp3toss|mp3drug|theindexof|index_of|wallywashis|indexofmp3)'
       }
+    }
+  },
+  validations: {
+    keyword: {
+      required,
+      minLength: minLength(1)
+    },
+    filetype: {
+      required
+    }
+  },
+  methods: {
+    getFiletype: function() {
+      const type = this.filetype
+      switch (type) {
+        case 'music':
+        default:
+          return this.api.formats = '(.ogg|.mp3|.flac|.wma|.m4a)'
+          break;
+        case 'flac':
+          return this.api.formats = '(.flac|.alac|.ape|.aac)'
+          break;
+        case 'video':
+          return this.api.formats = '(.mkv|.mp4|.avi|.mov|.mpg|.wmv)'
+          break;
+        case 'ebook':
+          return this.api.formats = '(.MOBI|.CBZ|.CBR|.CBC|.CHM|.EPUB|.FB2|.LIT|.LRF|.ODT|.PDF|.PRC|.PDB|.PML|.RB|.RTF|.TCR)'
+          break;
+        case 'kindle':
+          return this.api.formats = '(.MOBI|.EPUB|.LIT)'
+          break;
+        case 'image':
+          return this.api.formats = '(.jpg|.gif|.png|.tif|.tiff|.psd)'
+          break;
+        case 'archive':
+          return this.api.formats = '(.rar|.tar|.zip|.sit)'
+          break;
+      }
+    },
+    handleSubmit: function(searchParams) {
+      this.reformattedKeyword = searchParams.join(' ')
+      this.api.keyword = searchParams.join('+')
+      
+      const { baseUrl, formats, keyword, inTitle, inUrl } = this.api
+      const apiUrl = `${baseUrl}+${formats}+${keyword}+${inTitle}+${inUrl}`
+
+      console.log(apiUrl)
     }
   }
 }
