10e6b8d951814f5710f9ca792e98614cf5301ca4
webdevsuperfast <acob.rotsen@hotmail.com>
2019-11-03 15:18:38 +0800

    Added logo component

---
+++ src/components/Logo.vue
@@ -0,0 +1,14 @@
+<template>
+  <img :src="`${publicPath}images/logo.svg`" width="100" height="100" />
+</template>
+
+<script>
+export default {
+  name: 'Logo',
+  data() {
+    return {
+      publicPath: process.env.BASE_URL
+    }
+  }
+}
+</script>
