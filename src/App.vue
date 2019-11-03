003ac1fe06a29e68e0be6733a70be6934b16fc34
webdevsuperfast <acob.rotsen@hotmail.com>
2019-11-03 15:18:47 +0800

    Imported logo component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app" class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <Logo />
     <Search />
   </div>
 </template>
 
 <script>
 import Search from '@/components/Search'
+import Logo from '@/components/Logo'
 
 export default {
   name: 'app',
   components: {
-    Search
+    Search,
+    Logo
   }
 }
 </script>
