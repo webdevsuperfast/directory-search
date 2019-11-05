258e08450d8636b542ae63da788a4ddccf9426df
webdevsuperfast <acob.rotsen@hotmail.com>
2019-11-05 08:12:14 +0800

    Added meta information

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,23 @@ import Logo from '@/components/Logo'
 
 export default {
   name: 'app',
+  metaInfo: {
+    title: 'Open Directory Search',
+    titleTemplate: '% - Find Almost Anything You Want',
+    meta: {
+      description: 'Search open directories. Take advantage of Google\'s powerful search engine.',
+      keywords: [
+        'open directory',
+        'search engine',
+        'direct download',
+        'music',
+        'ebook',
+        'video',
+        'software',
+        'archive'
+      ] 
+    }
+  },
   components: {
     Search,
     Logo
