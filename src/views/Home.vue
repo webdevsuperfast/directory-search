564461721b4c112e0f02f5e6024eb1f2b1dbb2ae
webdevsuperfast <acob.rotsen@hotmail.com>
2019-11-05 15:09:50 +0800

    Added Git component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,21 @@
   <main class="mx-auto my-auto">
     <Logo />
     <Search />
+    <Git />
   </main>
 </template>
 
 <script>
 import Logo from '@/components/Logo'
 import Search from '@/components/Search'
+import Git from '@/components/Git'
 
 export default {
   name: 'Home',
   components: {
     Logo,
-    Search
+    Search,
+    Git
   }
 }
 </script>
