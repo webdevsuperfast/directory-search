d44dac2870114c71febaf831ae85b79da7e4c403
webdevsuperfast <acob.rotsen@hotmail.com>
2019-11-05 14:51:33 +0800

    Added Home view

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <main class="mx-auto my-auto">
+    <Logo />
+    <Search />
+  </main>
+</template>
+
+<script>
+import Logo from '@/components/Logo'
+import Search from '@/components/Search'
+
+export default {
+  name: 'Home',
+  components: {
+    Logo,
+    Search
+  }
+}
+</script>
