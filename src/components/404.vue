ff8c1a2cd5879b3f92d0fcedc1de5966b9bfc935
webdevsuperfast <acob.rotsen@hotmail.com>
2019-11-05 14:54:19 +0800

    Updated content

--- src/components/404.vue
+++ src/components/404.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>hello World</div>
+  <main class="mx-auto my-auto">
+    <h2 class="mb-5">Not Found, Error 404</h2>
+    <p class="mb-0">The page you are looking for no longer exists. Perhaps you can return back to the <router-link to="/">homepage</router-link> and see if you can find what you are looking for.</p>
+  </main>
 </template>
 
 <script>
