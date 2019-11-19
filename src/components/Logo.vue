7d8007d1d1e69d3b83eeab053a35103ef1f1193c
webdevsuperfast <acob.rotsen@hotmail.com>
2019-11-19 16:00:29 +0800

    Simplify image path

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,17 +1,12 @@
 <template>
   <router-link to="/">
-    <b-img-lazy class="mb-4 d-none d-md-block" fluid :src="`${publicPath}images/logo_with_text.png`" alt="Open Directory Search"></b-img-lazy>
-    <b-img-lazy class="mb-4 d-block mx-auto d-md-none" :src="`${publicPath}images/logo.svg`" width="100" alt="Open Directory Search"></b-img-lazy>
+    <b-img-lazy class="mb-4 d-none d-md-block" fluid src="/images/logo_with_text.png" alt="Open Directory Search"></b-img-lazy>
+    <b-img-lazy class="mb-4 d-block mx-auto d-md-none" src="/images/logo.svg" width="100" alt="Open Directory Search"></b-img-lazy>
   </router-link>
 </template>
 
 <script>
 export default {
-  name: 'Logo',
-  data() {
-    return {
-      publicPath: process.env.BASE_URL
-    }
-  }
+  name: 'Logo'
 }
 </script>
