5e9a750358f1c2e7585ffcb336116dbc3d59d0e4
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2019-11-19 20:22:52 +0800

    Re-structure search form

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app" class="d-flex align-items-center">
     <b-container class="p-0" fluid>
       <Logo />
-      <Search />
+      <Search @search="getSearchString" @filetype="getFileTypeString" @url="addUrl" />
       <Git />
     </b-container>
   </div>
@@ -19,6 +19,66 @@ export default {
     Search,
     Git,
     Logo
+  },
+  data() {
+    return {
+      url: '',
+      urls: [],
+      api: {
+        baseURL: 'https://google.com/search?q=',
+        formats: '',
+        query: '',
+        inTitle: 'index.of',
+        inURL: '(jsp|pl|php|html|aspx|htm|cf|shtml|hypem|unknownsecret|sirens|writeups|trimediacentral|articlescentral|listen77|mp3raid|mp3toss|mp3drug|theindexof|index_of|wallywashis|indexofmp3)'
+      }
+    }
+  },
+  methods: {
+    getFileTypeString: function(type) {
+      switch (type) {
+        case 'music':
+          this.api.formats = '(ogg|mp3|flac|wma|m4a|flac|alac|ape|aac)'
+          break;
+        case 'video':
+          this.api.formats = '(mkv|mp4|avi|mov|mpg|wmv)'
+          break;
+        case 'ebook':
+          this.api.formats = '(mobi|cbz|cbr|cbc|chm|epub|fb2|lit|lrf|odt|pdf|prc|pdb|pml|rb|rtf|tcr)'
+          break;
+        case 'image':
+          this.api.formats = '(jpg|gif|png|tif|tiff|psd)'
+          break;
+        case 'archive':
+          this.api.formats = '(rar|tar|zip|sit|7z|iso|dmg|cab)'
+          break;
+        case 'cd':
+          this.api.formats = '(exe|apk|app|ipa|msi)'
+          break;
+        default:
+          this.api.formats = ''
+          break;
+      }
+    },
+    getSearchString: function(searchParams) {
+      this.api.query = searchParams.join(' ')
+    },
+    addUrl: function() {
+      const { baseURL, query, formats, inTitle, inURL } = this.api
+      let apiUrl, finalQuery
+      
+      if (formats){
+        finalQuery = `intext:"${query}" intitle:"${inTitle}" +${formats} -inurl:${inURL}`
+      } else {
+        finalQuery = `intext:"${query}" intitle:"${inTitle}" -inurl:${inURL}`
+      }
+
+      apiUrl = `${baseURL}`+encodeURIComponent(finalQuery)
+
+      this.url = apiUrl
+      
+      window.open(apiUrl)
+      // console.log(this.url)
+    }
   },
   metaInfo: {
     title: 'Open Directory Search',
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -5,16 +5,16 @@
         <b-form-row>
           <b-col lg="7">
             <label class="sr-only" for="search-input">Search String</label>
-            <b-form-input 
+            <b-form-input
               autofocus
-              name="keyword" 
+              name="keyword"
               size="lg"
-              v-model="keyword" 
-              id="search-input" type="text" 
-              :placeholder="'Search anything e.g. ' + placeholder" 
+              v-model="keyword"
+              id="search-input"
+              type="text"
+              :placeholder="'Search anything e.g. ' + placeholder"
               v-on:keydown.enter.prevent
-              @change="getQuery"
-              ></b-form-input>
+            ></b-form-input>
           </b-col>
           <b-col lg="3">
             <label class="sr-only" for="search-select"></label>
@@ -25,11 +25,16 @@
               :options="filetypes"
               v-model="filetype"
               @change="getTypePlaceholder"
-            >
-            </b-form-select>
+            ></b-form-select>
           </b-col>
           <b-col lg="2">
-            <b-button block size="lg" variant="primary" @click="addUrl" :disabled="isDisabled">Search</b-button>
+            <b-button
+              block
+              size="lg"
+              variant="primary"
+              :disabled="isDisabled"
+              @click="parseSearchString"
+            >Search</b-button>
           </b-col>
         </b-form-row>
       </b-form>
@@ -39,118 +44,77 @@
 
 <script>
 export default {
-  name: 'Search',
+  name: "Search",
   data() {
     return {
-      filetype: 'everything',
-      placeholder: '',
+      filetype: "everything",
+      placeholder: "",
+      keyword: "",
       url: '',
-      urls: [],
-      keyword: '',
       filetypes: {
-        everything: 'Everything',
-        music: 'Music',
-        video: 'Video',
-        ebook: 'E-Book',
-        image: 'Images',
-        archive: 'Archive',
-        cd: 'Games/Software' 
-      },
-      api: {
-        baseUrl: 'https://www.google.com/search?q=',
-        formats: '',
-        query: '',
-        inTitle: 'index.of',
-        inUrl: [
-          '(jsp|pl|php|html|aspx|htm|cf|shtml|hypem|unknownsecret|sirens|writeups|trimediacentral|articlescentral)',
-          '(listen77|mp3raid|mp3toss|mp3drug|theindexof|index_of|wallywashis|indexofmp3)'
-        ]
+        everything: "Everything",
+        music: "Music",
+        video: "Video",
+        ebook: "E-Book",
+        image: "Images",
+        archive: "Archive",
+        cd: "Games/Software"
       }
-    }
+    };
   },
   computed: {
     isDisabled: function() {
-      return this.keyword.length == 0
+      return this.keyword.length == 0;
     }
   },
   methods: {
-    getFiletype: function(type) {
-      switch (type) {
-        case 'music':
-          this.api.formats = '(ogg|mp3|flac|wma|m4a|flac|alac|ape|aac)'
-          break;
-        case 'video':
-          this.api.formats = '(mkv|mp4|avi|mov|mpg|wmv)'
-          break;
-        case 'ebook':
-          this.api.formats = '(mobi|cbz|cbr|cbc|chm|epub|fb2|lit|lrf|odt|pdf|prc|pdb|pml|rb|rtf|tcr)'
-          break;
-        case 'image':
-          this.api.formats = '(jpg|gif|png|tif|tiff|psd)'
-          break;
-        case 'archive':
-          this.api.formats = '(rar|tar|zip|sit|7z|iso|dmg|cab)'
-          break;
-        case 'cd':
-          this.api.formats = '(exe|apk|app|ipa|msi)'
-          break;
-        default:
-          this.api.formats = ''
-          break;
-      }
-    },
     addPlaceholder: function(type) {
       switch (type) {
-        case 'music':
-          this.placeholder = 'Beethoven'
+        case "music":
+          this.placeholder = "Beethoven";
           break;
-        case 'video':
-          this.placeholder = 'Big Buck Bunny'
+        case "video":
+          this.placeholder = "Big Buck Bunny";
           break;
-        case 'ebook':
-          this.placeholder = 'Epic of Gilgamesh'
+        case "ebook":
+          this.placeholder = "Epic of Gilgamesh";
           break;
-        case 'image':
-          this.placeholder = 'Tux'
+        case "image":
+          this.placeholder = "Tux";
           break;
-        case 'archive':
-          this.placeholder = 'Debian'
+        case "archive":
+          this.placeholder = "Debian";
           break;
-        case 'cd':
-          this.placeholder = 'SuperTuxCart'
+        case "cd":
+          this.placeholder = "SuperTuxCart";
           break;
         default:
-          this.placeholder = 'Debian Operating System'
+          this.placeholder = "Debian Operating System";
           break;
       }
     },
-    getQuery: function() {
-      this.api.query = this.keyword.replace(/ +/g, " ").trim()
+    parseSearchString: function() {
+      const trimmedSearchString = this.keyword.trim();
+
+      if (trimmedSearchString !== "") {
+        const searchParams = trimmedSearchString.split(/\s+/);
+        this.$emit("search", searchParams);
+        this.$emit("filetype", this.filetype)
+        this.$emit("url", this.url)
+        console.log(this.filetype + searchParams)      
+        this.keyword = "";
+        this.filetype = 'everything';
+        this.placeholder = 'Debian Operating System';
+      }
     },
     getTypePlaceholder: function() {
-      this.getFiletype(this.filetype)
-      this.addPlaceholder(this.filetype)  
-    },
-    addUrl: function() {
-      const { baseUrl, query, formats, inTitle, inUrl } = this.api
-      let apiUrl, finalQuery
-      
-      if (formats){
-        finalQuery = `intext:"${query}" intitle:"${inTitle}" +${formats} -inurl:${inUrl[0]} -inurl:${inUrl[1]}`
-      } else {
-        finalQuery = `intext:"${query}" intitle:"${inTitle}" -inurl:${inUrl[0]} -inurl:${inUrl[1]}`
-      }
-
-      apiUrl = `${baseUrl}`+encodeURIComponent(finalQuery)
-
-      this.url = apiUrl
-      
-      window.open(apiUrl)
-    },
+      // this.getFiletype(this.filetype);
+      this.addPlaceholder(this.filetype);
+    }
   },
   created() {
-    this.getFiletype(this.filetype)
-    this.addPlaceholder(this.filetype)
+    // this.getFiletype(this.filetype)
+    this.addPlaceholder(this.filetype);
   }
-}
+};
 </script>
